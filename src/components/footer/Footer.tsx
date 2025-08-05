78da851453fe12be3e3cbf561a99c9c54459baa6
ddifreju <jufreddi@gmail.com>
2025-08-05 10:59:41 -0300

    Inclusão de ToastAlert, configuração de postagens e perfil

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,19 @@
 import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import { type ReactNode, useContext } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
 
 function Footer() {
 
     let data = new Date().getFullYear()
 
-    return (
-        <>
+    const { usuario } = useContext(AuthContext)
+
+    let component: ReactNode
+
+    if (usuario.token !== "") {
+
+        component = (
+
             <div className="flex justify-center bg-indigo-900 text-white">
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
@@ -13,18 +21,24 @@ function Footer() {
                     </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
-                        <a href="https://www.linkedin.com/in/juliana-freddi/" target="_blank">
+                        <a href="https://www.linkedin.com/school/generationbrasil" target="_blank">
                             <LinkedinLogo size={48} weight='bold' />
                         </a>
-                        <a href="https://www.instagram.com/julianafreddi/" target="_blank">
+                        <a href="https://www.instagram.com/generationbrasil" target="_blank">
                             <InstagramLogo size={48} weight='bold' />
                         </a>
-                        <a href="https://www.facebook.com/juliana.freddi.7/" target="_blank">
+                        <a href="https://www.facebook.com/generationbrasil" target="_blank">
                             <FacebookLogo size={48} weight='bold' />
                         </a>
                     </div>
                 </div>
             </div>
+
+        )
+    }
+    return (
+        <>
+            { component }
         </>
     )
 }
